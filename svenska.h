191806b4278151b3483caaa1f9372f8bb25d8c58
kasperab <kasperab@yahoo.se>
2022-08-23 15:14:22 +0200

    La till svenska.h

---
+++ svenska.h
@@ -0,0 +1,68 @@
+#pragma once
+
+// C
+//#define auto //samma på svenska
+#define bryt break
+#define fall case
+#define kar char //karaktär
+#define konst const
+#define fortsätt continue
+#define förval default
+#define gör do
+#define dubbel double
+#define annars else
+#define uppräkn enum //uppräkningstyp
+//#define extern //samma på svenska
+#define flyt float
+#define för for
+#define gåtill goto
+#define om if
+#define hel int //heltal
+#define lång long
+//#define register //samma på svenska
+#define returnera return
+#define kort short
+#define signerad signed
+#define storlekav sizeof
+#define statisk static
+#define strukt struct //struktur
+#define växla switch
+#define typdef typedef
+#define förening union
+#define osignerad unsigned
+#define inget void
+#define labil volatile
+#define medans while
+
+// C++
+#define _hps _asm //hopsättning
+//#define bool //samma på svenska
+#define fånga catch
+#define klass class
+#define konst_gjut const_cast
+#define radera delete
+#define dynamiskt_gjut dynamic_cast
+//#define explicit //samma på svenska
+#define falsk false
+#define vän friend
+#define irad inline
+#define föränderlig mutable
+#define namnutrymme namespace
+#define ny new
+#define nollpkr nullptr
+#define operatör operator
+#define privat private
+#define skyddad protected
+#define publik public
+#define omtolka_gjut reinterpret_cast
+#define statiskt_gjut static_cast
+#define mall template
+#define detta this
+#define kasta throw
+#define sann true
+#define försök try
+#define typid typeid
+#define typnamn typename
+#define virtuell virtual
+#define använder using
+#define bkar_t wchar_t //bred karaktär
